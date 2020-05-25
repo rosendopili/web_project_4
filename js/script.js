aa23a03403fadedf90b0af47851f3640520aad18
rosendopili <rosendopili@gmail.com>
2020-05-25 19:06:38 -0400

    adding review 1 edits.

--- js/script.js
+++ js/script.js
@@ -4,26 +4,28 @@ let username = document.querySelector('.profile__username').innerHTML;
 let occupation = document.querySelector('.profile__occupation').innerHTML; 
 let form = document.querySelector('.modal__form');
 let saveButton = document.querySelector('.modal__save-button'); 
+let newUsername = document.querySelector('.modal__username'); 
+let newOccupation = document.querySelector('.modal__occupation'); 
+let closeIcon = document.querySelector('.modal__close'); 
+let editButton = document.querySelector('.profile__edit-button'); 
 
 function modal(){
-    modalContainer.style.display = 'flex';
+    modalContainer.classList.remove('modal__off');  
     body.style.overflowY = 'hidden'; 
 }
 
 function closeModal(){
-    modalContainer.style.display = 'none'; 
+    modalContainer.classList.add('modal__off'); 
     body.style.overflowY = 'scroll'; 
 }
 
 function editProfile(){
-    let newUsername = document.querySelector('.modal__username').value; 
-    let newOccupation = document.querySelector('.modal__occupation').value; 
-    
-    document.querySelector('.profile__username').innerHTML = newUsername; 
-    document.querySelector('.profile__occupation').innerHTML = newOccupation; 
+    document.querySelector('.profile__username').textContent = newUsername.value; 
+    document.querySelector('.profile__occupation').textContent = newOccupation.value; 
 
     closeModal(); 
 }
 
 saveButton.addEventListener('click', editProfile);
-
+closeIcon.addEventListener('click', closeModal); 
+editButton.addEventListener('click', modal); 
